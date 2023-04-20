8224137c94ce1381a8bd7968a96e360563d96964
Antonio Jesús González Fernández <antoniognzle@gmail.com>
2023-04-20 19:58:52 +0200

    Refactor controller and route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,7 @@
 import fastify, { FastifyInstance } from "fastify";
 import { Prisma, PrismaClient } from "@prisma/client";
-import { User } from "./types/user";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import fastifyCors from "@fastify/cors";
+import { authRoutes, testRoutes } from "./routes";
 
 const prisma = new PrismaClient({
   datasources: {
@@ -14,80 +12,10 @@ const prisma = new PrismaClient({
 });
 
 const server: FastifyInstance = fastify({ logger: true });
-server.register(fastifyCors, {
-  // your options here, if needed
-});
+server.register(fastifyCors, {});
 
-server.get("/", async (_request, reply) => {
-  return { hello: "world", nombre: "Antonio" };
-});
-
-server.post("/api/register", async (request, reply) => {
-  const { name, email, password } = request.body as User;
-  console.log(request.body);
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-  if (existingUser) {
-    reply.status(409).send({
-      message: "User with this email already exists",
-    });
-    return;
-  }
-  const newUser = await prisma.user.create({
-    data: {
-      email: email,
-      name: name,
-      password: hashedPassword,
-    },
-  });
-
-  console.log("User created:", newUser);
-
-  const token = jwt.sign(
-    { userId: newUser.id },
-    process.env.JWT_SECRET as string
-  );
-
-  reply.send({ message: "User registered successfully", token: token });
-});
-
-server.post("/api/login", async (request, reply) => {
-  const { email, password } = request.body as {
-    email: string;
-    password: string;
-  };
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-
-  if (!user) {
-    reply.status(401).send({
-      message: "Invalid email or password",
-    });
-    return;
-  }
-
-  const passwordMatches = await bcrypt.compare(password, user.password);
-
-  if (!passwordMatches) {
-    reply.status(401).send({
-      message: "Invalid email or password",
-    });
-    return;
-  }
-
-  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string);
-
-  reply.send({ token: token });
-});
+server.register(testRoutes);
+server.register(authRoutes);
 
 const start = async () => {
   if (!prisma.$connect) {
