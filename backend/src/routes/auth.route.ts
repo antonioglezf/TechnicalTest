ff0e2ae8b603f017cead78dde27a8168ec059574
Antonio Jesús González Fernández <antoniognzle@gmail.com>
2023-04-21 22:55:33 +0200

    Auth-flow test

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register, login } from "../controllers/auth.controller";
+import { validate } from "../middlewares/validate";
 
 function authRoutes(fastify: FastifyInstance, _options: any, done: () => void) {
   fastify.post("/api/register", register);
   fastify.post("/api/login", login);
+  fastify.get("/api/validate", validate);
   done();
 }
 
