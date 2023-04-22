560da4acc5459ca3be5bdfd8ed0c832966d7df4b
Antonio Jesús González Fernández <antoniognzle@gmail.com>
2023-04-22 15:39:11 +0200

    Clean unused

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,23 @@
-import fastify, {
-  FastifyInstance,
-  FastifyReply,
-  FastifyRequest,
-} from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 import fastifyCors from "@fastify/cors";
 import { authRoutes, projects, testRoutes } from "./routes";
-import { validate } from "./middlewares/validate";
 
-export async function Server() {
-  const server: FastifyInstance = fastify({ logger: true });
-  server.register(fastifyCors, {});
-  server.register(testRoutes);
-  server.register(authRoutes);
-  server.register(projects);
+const server: FastifyInstance = fastify({ logger: true });
+server.register(fastifyCors, {});
 
+server.register(testRoutes);
+server.register(authRoutes);
+
+const start = async () => {
   try {
     await server.listen({ port: 3000, host: "0.0.0.0" });
     const address = server.server.address();
-
     const port = typeof address === "string" ? address : address?.port;
     server.log.info(`Server listening on port ${port}`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
-}
+};
+
+start();
